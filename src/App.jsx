52e338d982df938cf40365ea2e4ee41d320f2d8e
Blacklion567 <naviedaj567@gmail.com>
2024-02-24 22:48:02 +0800

    08-Use-Fetch-Hook

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import Section01 from "./01-UserCard/Section01";
+// import Section02 from "./02-Array-Component/Section02";
+// import Section03 from "./03-Name-Counter/Section03";
+// import Section04 from "./04-Use-Effect-Exercises/Section04";
+// import Section05 from "./05-Lifecycle-Method-Exercises/Section05";
+// import Section06 from "./06-User-List/Section06";
+// import Section07 from './07-Simple-Todo-List/Section07';
+
+import Section08 from "./08-Use-Fetch-Hook/Section08";
+
+
+function App() {
+
+  return (
+    <>
+      {/* <Section01 />
+      <Section02 />
+      <Section03 />
+      <Section04 />
+      <Section05 />
+      <Section06 />
+      <Section07 />*/}
+      <Section08 />
+    </>
+  );
+}
+
+export default App;
