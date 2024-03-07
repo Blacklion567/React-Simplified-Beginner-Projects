5daff73541d227227b15909d3bf780c720da174f
Blacklion567 <naviedaj567@gmail.com>
2024-03-07 17:50:22 +0800

    BanDevSimplified

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,37 @@
 // import Section05 from "./05-Lifecycle-Method-Exercises/Section05";
 // import Section06 from "./06-User-List/Section06";
 // import Section07 from './07-Simple-Todo-List/Section07';
-
-import Section08 from "./08-Use-Fetch-Hook/Section08";
-
+// import Section08 from "./08-Use-Fetch-Hook/Section08";
+// import Section09 from "./09-Use-Array-Hook/Section09";
+// import Section10 from "./10-Use-Local-Storage-Hook/Section10";
+// import Section11 from "./11-DigitalClock/Section11";
+// import Section12 from './12-StopWatch/Section12';
+// import Section14 from './14-React-Hook-Form-Project/Section14';
+// import Section13 from './13-Basic-form-validation-project/Section13';
+import Section15 from './15-Advanced-Todo-List/Section15';
 
 function App() {
-
-  return (
-    <>
-      {/* <Section01 />
+   return (
+      <>
+         {/*
+      <Section01 />
       <Section02 />
       <Section03 />
       <Section04 />
       <Section05 />
       <Section06 />
-      <Section07 />*/}
+      <Section07 />
       <Section08 />
-    </>
-  );
+      <Section09 />
+      <Section10 />
+      <Section11 />
+      <Section12 />
+      <Section13 />
+      <Section14 />
+   */}
+         <Section15 />
+      </>
+   );
 }
 
 export default App;
